5730c287aa0227cb3250502a3c52bb15eea07869
Jasleen <Jasleen@LAPTOP-IREO6BKM>
2017-07-28 13:56:01 -0400

    Programm updated

--- assignment04-300929376/Program.cs
+++ assignment04-300929376/Program.cs
@@ -16,19 +16,18 @@ namespace assignment04_300929376
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\n @-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@- PLANET INFORMATION @-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@\n");
+            Console.WriteLine("######################################### PLANET INFORMATION ###################################################");
+            Console.WriteLine("############################################-JASLEEN- 300929376 ################################################");
             Console.WriteLine("LET'S KNOW ABOUT PLANETS");
-            Console.WriteLine("\n \n  ");
             Console.WriteLine("A Planet is a celestial body distinguished from the fixed stars by having an apparent motion of its own (including the moon and sun), especially with reference to its supposed influence on people and events.");
-            Console.WriteLine("\n\n");
+
             Console.WriteLine("1) Mercury,\n\n\t2) Venus,\n\n\t\t3) Earth,\n\n\t\t\t4) Mars,\n\n\t\t\t\t5) Jupiter,\n\n\t\t\t\t\t 6)Saturn,\n\n\t\t\t\t\t\t7)Uranus, \n\n\t\t\t\t\t\t\t8)Neptune");
-            Console.WriteLine("\n @-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@-@\n");
+            Console.WriteLine("#############################################################################################################");
             GiantPlanet giantPlanet = new GiantPlanet("Venus", 15674, 617.8, "GAS");
-            Console.WriteLine("\n");
-            
-            TerrestrialPlanet terrestrialPlanet = new TerrestrialPlanet("Mercury", 14563, 6.3452, true);
+           TerrestrialPlanet terrestrialPlanet = new TerrestrialPlanet("Mercury", 14563, 6.3452, true);
             giantPlanet.ToString();
             terrestrialPlanet.ToString();
+            Console.WriteLine("#############################################################################################################");
             Program.WAITFORANYKEY();
         }
         static void WAITFORANYKEY()
